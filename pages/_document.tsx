f3d8a5d1c1267a4d97620c989ebb280bbfe565ae
lordgent1 <lordgent@digitalcenter.id>
2024-08-16 01:06:58 +0700

    update head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="icon" href="/assets/icons/logo.svg" type="image/svg+xml" />
+      <title>Moment Kita</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
